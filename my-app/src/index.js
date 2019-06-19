3520b7b540e8ebeadf468e2e1342499a46125679
jpinaProfile <jpina@profile.es>
2019-06-19 18:34:59 +0200

    component card

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './components/Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+//ponemos asi las imagenes para poder cargarlas
+import aguacate from './assets/images/3069191_0.jpg'; 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <div>
+        <Card id='0' name='aguacate' routeImage={aguacate}/> 
+        <Card id='0' name='aguacate' routeImage={aguacate}/>      
+    </div>
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
